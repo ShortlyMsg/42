434bbbc6b546356652960b1a4eed07a97d176045
ShortlyMsg <42msg42@gmail.com>
2023-04-25 09:00:55 +0300

    42 Piscine | c02

---
+++ c02/ex01/ft_strncpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgurbuz <mgurbuz@student.42kocaeli.com.tr  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/08 14:05:22 by mgurbuz           #+#    #+#             */
+/*   Updated: 2023/04/08 14:05:26 by mgurbuz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
---
+++ c02/ex09/ft_strcapitalize.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgurbuz <mgurbuz@student.42kocaeli.com.tr  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/08 16:28:24 by mgurbuz           #+#    #+#             */
+/*   Updated: 2023/04/08 16:28:30 by mgurbuz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+	int	x;
+
+	i = 0;
+	x = 1;
+	ft_strlowcase(str);
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (x == 1)
+				str[i] -= 32;
+			x = 0;
+		}
+		else if (str[i] >= '0' && str[i] <= '9')
+			x = 0;
+		else
+			x = 1;
+		i++;
+	}
+	return (str);
+}
